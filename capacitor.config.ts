d7ff22e7f3959ea40756b6cb3aafff5597c725c8
MrFlexi <szaroletta@hotmail.com>
2024-07-10 19:11:27 +0200

    Init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SurfCam',
+  webDir: 'www'
+};
+
+export default config;
